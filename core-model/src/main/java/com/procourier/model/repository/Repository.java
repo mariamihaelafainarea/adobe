95379c1a2cb60f7ff7f0f08382391428fea08e51
mariamihaelafainarea <mariamihaelafainarea4@gmail.com>
2019-03-21 19:04:27 +0200

    Working on

--- core-model/src/main/java/com/procourier/model/repository/Repository.java
+++ core-model/src/main/java/com/procourier/model/repository/Repository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface Repository<T> {
-    Optional<T> findById(T id);
+    Optional<T> findById(Long id);
     List<T> getAll();
     Long put(T item);
     List<Long> putAll(List<T> items);
--- core-model/src/main/java/module-info.java
+++ core-model/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module core.model {
     //vrem sa fie exprotat spre exterior acest modul :)
     exports com.procourier.model;
+    exports com.procourier.model.repository;
 
 }
